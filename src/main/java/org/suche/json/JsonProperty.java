09be33df9f8907b86f2adaedf0aa200a1c65d86e
SkateScout <SkateScout@gmail.com>
2026-06-23 09:08:16 +0200

    Missing Property

---
+++ src/main/java/org/suche/json/JsonProperty.java
@@ -0,0 +1,12 @@
+package org.suche.json;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JsonProperty {
+	/** @return alternative name for the field/getter/setter/parameter  */ String   value() default "";
+}
