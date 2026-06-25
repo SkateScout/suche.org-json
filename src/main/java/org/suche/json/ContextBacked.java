7e02b1e0f3edf35c771b6dd4c1bfd38a993f0e52
SkateScout <SkateScout@gmail.com>
2026-06-26 00:21:03 +0200

    Out of Bound error Messages

--- src/main/java/org/suche/json/ContextBacked.java
+++ src/main/java/org/suche/json/ContextBacked.java
@@ -9,14 +9,25 @@ sealed interface ContextBacked permits CompactList, CompactMap, JSONArray {
 	default long      getLong   (final int idx) {
 		final var prims      = prims     ();
 		final var singleType = singleType();
-		if(idx < 0 || idx >= size()) JsonEngine.illegalStateException("Index "+idx+" not in [0..."+(prims.length-1)+"]");
 		return switch(singleType) {
-		case PRIMITIVE.T_LONG   -> prims[idx];
-		case PRIMITIVE.T_DOUBLE -> (long)Double.longBitsToDouble(prims[idx]);
+		case PRIMITIVE.T_LONG   -> {
+			if(idx < 0 || idx >= size()) JsonEngine.illegalStateException("Index "+idx+" not in [0...S:"+size()+"<C:"+(prims.length-1)+"]");
+			yield prims[idx];
+		}
+		case PRIMITIVE.T_DOUBLE -> {
+			if(idx < 0 || idx >= size()) JsonEngine.illegalStateException("Index "+idx+" not in [0...S:"+size()+"<C:"+(prims.length-1)+"]");
+			yield (long)Double.longBitsToDouble(prims[idx]);
+		}
 		default -> {
 			final var data = rawValueAt(idx);
-			if(data == PRIMITIVE.LONG  ) yield prims[idx];
-			if(data == PRIMITIVE.DOUBLE) yield (long)Double.longBitsToDouble(prims[idx]);
+			if(data == PRIMITIVE.LONG  ) {
+				if(idx < 0 || idx >= size()) JsonEngine.illegalStateException("Index "+idx+" not in [0...S:"+size()+"<C:"+(prims.length-1)+"]");
+				yield prims[idx];
+			}
+			if(data == PRIMITIVE.DOUBLE) {
+				if(idx < 0 || idx >= size()) JsonEngine.illegalStateException("Index "+idx+" not in [0...S:"+size()+"<C:"+(prims.length-1)+"]");
+				yield (long)Double.longBitsToDouble(prims[idx]);
+			}
 			if(data instanceof final Number t) yield t.longValue();
 			throw JsonEngine.illegalStateException("Index["+idx+"] nonPrimitive");
 		}
